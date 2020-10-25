1ed3632ef9b88484cac1fb53aa6b5f46259ec775
Jürgen Probst <juergen.probst@gmail.com>
2020-10-25 19:05:07 +0100

    First Code

---
+++ FormLetter.py
@@ -0,0 +1,190 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct  1 08:38:16 2020
+
+@author: Jürgen Probst
+"""
+import pandas as pd
+import jinja2
+import babel.numbers, babel.dates
+import weasyprint
+import os.path
+import sys, os
+
+
+# Jinja2 documentation: https://jinja.palletsprojects.com/en/2.11.x/
+# https://jinja.palletsprojects.com/en/2.11.x/api/
+# https://jinja.palletsprojects.com/en/2.11.x/templates/
+
+# http://babel.pocoo.org/en/latest/numbers.html#numbers
+# http://babel.pocoo.org/en/latest/dates.html#date-and-time
+
+# different ways to center text in html: https://www.computerhope.com/issues/ch001474.htm
+# https://weasyprint.readthedocs.io/en/stable/tips-tricks.html
+# Optional whitespace escaping: https://svn.python.org/projects/external/Jinja-1.1/docs/build/escaping.html
+
+
+class FormLetter(object):
+
+    def __init__(self, template, datafile, sheet_name=None):
+        """Create a FormLetter object.
+
+        :param template: filename of template file (.html file) which will
+            be filled by the datafile table entries.
+
+        :param datafile: filename of table file (e.g. .csv or .xlsx) which
+            will be used to fill template.
+
+        :param sheet_name: Sheet name of datafile to be used, if .xlsx file.
+            If None (default), the first sheet will be used.
+
+        """
+        self.template_file = template
+
+        self.datafile = datafile
+        self.data_ext = os.path.splitext(datafile)[-1]
+        if self.data_ext == '.xlsx':
+            # open excel file
+            xl = pd.ExcelFile(datafile)
+            if sheet_name is None:
+                self.sheet_name = xl.sheet_names[0]
+            elif sheet_name in xl.sheet_names:
+                self.sheet_name = sheet_name
+            else:
+                raise ValueError("sheet name '%s' not in file" % sheet_name)
+            self.data = xl.parse(self.sheet_name)
+        else:
+            try:
+                self.data = pd.read_csv(datafile)
+            except pd.errors.ParserError as pe:
+                print("unknown data file format")
+                raise pe
+
+        # find column names with spaces:
+        columns = list(self.data.columns)
+        for i, column in enumerate(columns):
+            if " " in column:
+                new_name = column.replace(" ", "_")
+                print('WARNING: data column name "%s" contains spaces. '
+                      'Will be renamed to "%s". '
+                      'Please use new name in template' % (column, new_name))
+                columns[i] = new_name
+        self.data.columns = columns
+
+        # prepare substitution dictionary, will be used for every row:
+        self.subdict = {key: "" for key in self.data.columns}
+
+        self.env = jinja2.Environment(
+                loader=jinja2.FileSystemLoader(searchpath="./"))
+
+        # On a tested windows machine, babel wouldn't work because there
+        # was no current locale set. Apparently, there is no `LC_NUMERIC` 
+        # environment variable, so the babel default locale of
+        # `babel.default_locale('LC_NUMERIC')` returns `None`
+        # Solution: Set the environment variable 'LC_ALL' since it will
+        # also help with the non-existing 'LC_TIME' for example:
+        self.locale = locale.getlocale()[0]
+        if babel.default_locale('LC_NUMERIC') is None:
+            if os.getenv('LC_ALL') is None:
+                os.environ['LC_ALL'] = self.locale
+
+        # Add some formatters to Jinja environment, so they can be
+        # used in the template:
+        self.env.filters['format_currency'] = babel.numbers.format_currency
+        self.env.filters['format_percent'] = babel.numbers.format_percent
+        self.env.filters['format_decimal'] = babel.numbers.format_decimal
+        self.env.filters['format_amount'] = lambda x: babel.numbers.format_decimal(
+            x, format=u'#,##0.00')
+        self.env.filters['format_date'] = babel.dates.format_date
+        self.env.filters['format_datetime'] = babel.dates.format_datetime
+        # TODO self.env.filters['format_adapted_date'] = date_formatter
+
+        self.template = self.env.get_template(self.template_file)
+
+        print(self.data.columns) # TODO debugging only
+        print(self.data.head())
+        print()
+        print()
+
+
+    def get_filled_html(self, row,
+            #TODO custom_formatters=None,
+            #TODO locale=None
+            ):
+        """Return the template, filled with the data of the specified row,
+        as HTML-formatted string.
+
+        :param row:
+            the row number of data which will be used to fill the template.
+            start counting at 0.
+        :param custom_formatters: None or dict of one-parameter functions;
+            If None (default), a set of default formatters for each
+            column will be used, using babel.numbers and babel.dates.
+            Individual formatting functions can be supplied with the
+            column names as keys. The result of each function
+            must be a unicode string.
+        :param locale: None or locale identifier, e.g. 'de_DE' or 'en_US';
+            The locale used for formatting numeric and date values with
+            babel. If None (default), the locale will be taken from the
+            `LC_NUMERIC` or `LC_TIME` environment variables on your
+            system, for numeric or date values, respectively.
+        :returns:
+            HTML-formatted string
+
+        """
+        # prepare substitution dictionary for current row:
+        self.subdict.update(self.data.iloc[row].items())
+        html = self.template.render(self.subdict)
+
+        return html
+
+
+    def write_to_pdf(self, row, file_name):
+        """Save the template, filled with the data of the specified row,
+        as PDF file
+
+        :param row:
+            the row number of data which will be used to fill the template.
+            start counting at 0.
+        :param file_name: string;
+            Destination file name.
+
+        """
+        html = self.get_filled_html(row)
+        doc = weasyprint.HTML(string=html)
+        doc.write_pdf(file_name)
+
+    def get_data_row(self, row):
+        return self.data.iloc[row]
+
+    def get_number_of_rows(self):
+        return self.data.shape[0]
+
+def main(argv=sys.argv[1:]):
+    if len(argv) < 2:
+        print("usage: python Formletter.py templatefile.html datafile.xlsx "
+              "[sheet_name]")
+        return
+    print('using template file: %s' % argv[0])
+    print('using data file: %s' % argv[1])
+    if len(argv) > 2:
+        print('using sheet name: %s' % argv[2])
+        fl = FormLetter(argv[0], argv[1], argv[2])
+    else:
+        fl = FormLetter(argv[0], argv[1])
+
+    total = fl.get_number_of_rows()
+    for i in range(total):
+        row = fl.get_data_row(i)
+        if row["1_wenn_RN_verschickt"]:
+            print("skipping %i/%i: %s %s" % (i + 1, total, row["RN"], row["Person"]))
+            continue
+        fname = "pdf%0i_%s_%s.pdf" % (i, row["RN"], row["Person"])
+        print("procesing %i/%i: file %s" % (i + 1, total, fname))
+        fl.write_to_pdf(i, fname)
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ FormLetter_GUI.py
@@ -0,0 +1,277 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 21 01:19:16 2020
+
+@author: JÃƒÂ¼rgen Probst
+
+"""
+
+import sys, os
+import tkinter as tk
+from tkinter import ttk
+from tkinter import filedialog
+from tkinter import messagebox
+import pandas as pd
+#import xlrd
+#import FormLetter
+
+
+class PlaceholderEntry(tk.Entry):
+    def __init__(self, master=None, placeholdertext="", color='grey', **kwargs):
+        super().__init__(master, **kwargs)
+
+        self.placeholder_text = placeholdertext
+        self.placeholder_color = color
+        self.default_fg_color = self['fg']
+
+        self.bind("<FocusIn>", self.focus_in)
+        self.bind("<FocusOut>", self.focus_out)
+
+        self.write_placeholder()
+
+    def write_placeholder(self):
+        self.insert(0, self.placeholder_text)
+        self['fg'] = self.placeholder_color
+
+    def focus_in(self, *args):
+        if self['fg'] == self.placeholder_color:
+            self.delete('0', tk.END)
+            self['fg'] = self.default_fg_color
+
+    def focus_out(self, *args):
+        if not self.get():
+            self.write_placeholder()
+
+
+
+class Application(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.pack(fill=tk.BOTH, expand=1, padx=5, pady=5)
+        self.create_widgets()
+
+    def create_widgets(self):
+        style = ttk.Style()
+        style.theme_use('clam')
+        style.map(
+            'custom.TCombobox',
+            fieldbackground=[('readonly','white'), ('disabled', 'gray80')],
+            foreground=[('readonly','black')],
+            # hide selection when out-of-focus:
+            selectbackground=[('readonly', '!focus', 'white')],
+            selectforeground=[('readonly', '!focus', 'black')]
+            )
+        style.map(
+            'TEntry',
+            fieldbackground=[('disabled', 'gray80')])
+        style.configure("custom.TButton", padding=2)
+        # https://stackoverflow.com/questions/24768090/progressbar-in-tkinter-with-a-label-inside#40348163
+        style.layout("TProgressbar",
+         [('TProgressbar.trough',
+           {'children': [('LabeledProgressbar.pbar',
+                          {'side': 'left', 'sticky': 'ns'}),
+                         ("LabeledProgressbar.label",
+                          {"sticky": ""})],
+           'sticky': 'nswe'})])
+
+        fileframe = tk.LabelFrame(self, text="Files", pady=5, padx=5)
+        fileframe.pack(side=tk.TOP, fill=tk.X, expand=0)
+
+        topframe = tk.Frame(fileframe, pady=0, padx=0)
+        topframe.pack(side=tk.TOP, fill=tk.X, expand=0)
+
+        tk.Label(topframe, text="Template file (*.html):   ").pack(side=tk.LEFT)
+        self.templatefile_edt = ttk.Entry(topframe, width=1)
+        self.templatefile_edt.pack(side=tk.LEFT, fill=tk.X, expand=1, ipady=2)
+        self.templatefile_edt.bind('<Return>', self.templatefile_edt_return)
+        ttk.Button(
+            topframe, text="...", style="custom.TButton", width=4,
+            command=self.open_template_dialog).pack(
+                side=tk.LEFT, fill=tk.X)
+
+        topframe = tk.Frame(fileframe, pady=5, padx=0)
+        topframe.pack(side=tk.TOP, fill=tk.X, expand=0)
+
+        tk.Label(topframe, text="Data file (*.xlsx / *.csv): ").pack(side=tk.LEFT)
+        self.datafile_edt = ttk.Entry(topframe, width=1)
+        self.datafile_edt.pack(side=tk.LEFT, fill=tk.X, expand=1, ipady=2)
+        self.datafile_edt.bind('<Return>', self.datafile_edt_return)
+        ttk.Button(
+            topframe, text="...", style="custom.TButton", width=4,
+            command=self.open_data_dialog).pack(
+                side=tk.LEFT, fill=tk.X)
+        tk.Label(topframe, text="  Sheet: ").pack(side=tk.LEFT)
+        self.sheets_combo = ttk.Combobox(
+                topframe, values=[], width=1,
+                state='disabled', style="custom.TCombobox")
+        self.sheets_combo.pack(side=tk.LEFT, fill=tk.X, expand=1, ipady=2)
+        self.sheets_combo.bind('<<ComboboxSelected>>', self.update_sheet)
+
+
+        tk.Label(self, text="").pack(side=tk.TOP)
+        #ttk.Separator(self, orient="horizontal").pack(side=tk.TOP, pady=5)
+
+        frame = tk.LabelFrame(self, text="Options", pady=5, padx=5)
+        frame.pack(side=tk.TOP, fill=tk.X, expand=0)
+
+        tk.Label(frame, text="Skip dataset if ").pack(side=tk.LEFT)
+        self.skip_combo = ttk.Combobox(
+                frame, values=[], width=1,
+                state='disabled', style="custom.TCombobox")
+        self.skip_combo.pack(side=tk.LEFT, fill=tk.X, expand=1, ipady=2, pady=5)
+        tk.Label(frame, text=" equals ").pack(side=tk.LEFT)
+        self.skip_edt = ttk.Entry(frame, width=1, state='disabled')
+        self.skip_edt.pack(side=tk.LEFT, fill=tk.X, expand=1, ipady=2)
+
+
+
+        tk.Label(self, text="").pack(side=tk.TOP)
+        frame = tk.LabelFrame(self, text="Output", pady=5, padx=5)
+        frame.pack(side=tk.TOP, fill=tk.X, expand=0)
+
+        tk.Grid.columnconfigure(frame, 1, weight=1)
+        tk.Grid.columnconfigure(frame, 3, weight=1)
+        tk.Label(frame, text="Save pdf-files in: ").grid(row=0, column=0, sticky='w') #pack(side=tk.LEFT)
+        self.dir_edt = ttk.Entry(frame, width=1, state='disabled')
+        self.dir_edt.grid(row=0, column=1, sticky='nwse', columnspan=3) #.pack(side=tk.LEFT, fill=tk.X, expand=1, pady=2)
+        ttk.Button(
+            frame, text="...", style="custom.TButton", width=4, state='disabled',
+            command=self.open_template_dialog).grid(row=0, column=4)  #pack(side=tk.LEFT, fill=tk.X)
+
+
+        var = tk.IntVar()
+
+        tk.Label(frame, text="").grid(row=1)
+       # rframe = tk.Frame(frame, pady=5, padx=5)
+   #     rframe.pack(side=tk.TOP, fill=tk.X, expand=0)
+        r1 = tk.Radiobutton(frame, text='convert all', variable=var, value=1)
+        r1.grid(row=2, column=0, sticky='w') #pack(side=tk.TOP)
+        r2 = tk.Radiobutton(frame, text='convert range:', variable=var, value=2)
+        r2.grid(row=3, column=0, sticky='w')
+        r3 = tk.Radiobutton(frame, text='convert selection: ', variable=var, value=3)
+        r3.grid(row=4, column=0, sticky='w')
+        tk.Spinbox(frame, from_=1, to=1, bg='white', state='normal').grid(row=3, column=1, sticky='we')
+        tk.Label(frame, text='to').grid(row=3, column=2, sticky='w')
+        tk.Spinbox(frame, from_=1, to=1, bg='white', state='normal').grid(row=3, column=3, sticky='we')
+        PlaceholderEntry(frame, " for example: 1, 3-5, 7, 9", bg='white').grid(
+                row=4, column=1, sticky='we', columnspan=3)
+        var.set(1)
+        r1.invoke()
+
+
+
+        tk.Label(self, text="").pack(side=tk.TOP)
+        frame = tk.LabelFrame(self, text="Progress", pady=5, padx=5)
+        frame.pack(side=tk.TOP, fill=tk.X, expand=0)
+
+        p1 = ttk.Progressbar(frame, value=5, maximum=300,
+                     mode="determinate",
+                     #orient=tk.HORIZONTAL,
+                     style="TProgressbar")
+        p1.pack(side=tk.LEFT, fill=tk.X, expand=1)
+        # change the text of the progressbar,
+        # the trailing spaces are here to properly center the text
+        style.configure("TProgressbar", text="0 %      ")
+        style.configure("TProgressbar", text="{0} %      ".format(76))
+        style.configure("TProgressbar", text="10/2345       ")
+
+
+
+        #tk.Label(self, text="").pack(side=tk.TOP)
+        #tk.Label(self, text="").pack(side=tk.BOTTOM)
+        frame = tk.Frame(self, pady=5, padx=5)
+        frame.pack(side=tk.BOTTOM, fill=tk.Y, expand=1, anchor="center")
+
+        ttk.Button(
+            frame, text="Go!", style="custom.TButton",
+            command=self.open_template_dialog).pack(
+                side=tk.LEFT, fill=tk.X)
+        tk.Label(frame, text=" ").pack(side=tk.LEFT, padx=5)
+        ttk.Button(
+            frame, text="Exit", style="custom.TButton",
+            command=self.open_template_dialog).pack(
+                side=tk.LEFT, fill=tk.X)
+
+    def templatefile_edt_return(self, event):
+        if not self.templatefile_edt.get():
+            # no filename entered yet:
+            self.open_template_dialog()
+        else:
+            # do nothing, filename will be read later
+            pass
+
+    def datafile_edt_return(self, event):
+        if not self.datafile_edt.get():
+            # no filename entered yet:
+            self.open_data_dialog()
+        else:
+            self.open_data_file(self.datafile_edt.get())
+
+    def open_template_dialog(self):
+        fname = filedialog.askopenfilename(
+            filetypes=[('HTML', '*.html'), ('all files', '*.*')])
+        if fname:
+            self.templatefile_edt.delete(0, tk.END)
+            self.templatefile_edt.insert(0, fname)
+
+    def open_data_dialog(self):
+        fname = filedialog.askopenfilename(
+            filetypes=[('Excel files', '*.xlsx *.xls *.csv'), ('all files', '*.*')])
+        if fname:
+            self.datafile_edt.delete(0, tk.END)
+            self.datafile_edt.insert(0, fname)
+            self.open_data_file(fname)
+
+    def open_data_file(self, fname):
+        self.datafilename = fname
+        ext = os.path.splitext(fname)[-1]
+        if ext in ['.xlsx', '.xls']:
+            # open excel file
+            self.xlbook = pd.ExcelFile(fname)
+            self.sheet_names = self.xlbook.sheet_names
+            self.sheets_combo.config(state='readonly')
+            self.sheets_combo["values"] = self.sheet_names
+            self.sheets_combo.current(newindex=0)
+            self.update_sheet()
+        else:
+            self.sheets_combo.set("")
+            self.sheets_combo.config(state='disabled')
+            self.sheets_combo["values"] = []
+            self.xlbook = None
+            self.sheet_names = self.sheet_name = None
+            try:
+                self.data = pd.read_csv(fname)
+                self.update_data_columns()
+            except pd.errors.ParserError as pe:
+                print("unknown data file format")
+                raise pe
+
+    def update_sheet(self, event=None):
+        #if event is not None:
+        #    event.widget.selection_clear()
+        self.sheet_name = self.sheets_combo.get()
+        self.data = self.xlbook.parse(self.sheet_name)
+        self.update_data_columns()
+
+    def update_data_columns(self):
+        self.data_columns = list(self.data.columns)
+        print(self.data_columns)
+
+def main():
+    root = tk.Tk()
+    app = Application(master=root)
+    # set window title
+    root.title("FormLetter")
+    root.geometry("640x600")
+
+    def on_closing():
+        #if messagebox.askokcancel("Quit", "Do you want to quit?"):
+        root.destroy()
+
+    root.protocol("WM_DELETE_WINDOW", on_closing)
+    app.mainloop()
+
+if __name__ == '__main__':
+    main()
